c69c9790c16655622ee6925b3aaffa7ac4c702a4
Jody Albritton <jody.albritton@gmail.com>
2014-11-18 23:20:56 -0600

    fix menus

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -36,7 +36,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.html { redirect_to locations_path, notice: 'Location was successfully created.' }
         format.json { render :show, status: :created, location: @location }
       else
         format.html { render :new }
@@ -50,7 +50,7 @@ class LocationsController < ApplicationController
   def update
     respond_to do |format|
       if @location.update(location_params)
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.html { redirect_to locations_path, notice: 'Location was successfully updated.' }
         format.json { render :show, status: :ok, location: @location }
       else
         format.html { render :edit }
