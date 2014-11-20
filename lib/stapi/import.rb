c802a8048e6f5f231dfed84d2dcc7997f2f226bb
Jody Albritton <jody.albritton@gmail.com>
2014-11-19 19:30:14 -0600

    fix import

--- lib/stapi/import.rb
+++ lib/stapi/import.rb
@@ -1,11 +1,22 @@
 module Stapi
 	module Import
 		def import
-             
-			 devices = switches.parsed_response + dimmers.parsed_response + contacts.parsed_response + motions.parsed_response + illuminants.parsed_response + temperatures.parsed_response + humidities.parsed_response + locks.parsed_response + batteries.parsed_response
-           
+             swits = switches.parsed_response
+			 dims = dimmers.parsed_response 
+			 conts = contacts.parsed_response  
+			 mots = motions.parsed_response 
+			 illums = illuminants.parsed_response 
+			 temps = temperatures.parsed_response 
+			 hums = humidities.parsed_response 
+			 locs = locks.parsed_response 
+			 bats = batteries.parsed_response
+
+			
+
+             unless locs == nil 
              @user = User.find(@thing_user)
-              devices.each do |it|
+             devices = locs 
+             devices.each do |it|
         
 			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
 			        stuff.uid = it["id"]
@@ -16,7 +27,127 @@ module Stapi
 					stuff.save
 			  end
 			
-
+			end
+			unless dims == nil 
+             @user = User.find(@thing_user)
+             devices = dims 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end
+			unless swits == nil 
+             @user = User.find(@thing_user)
+             devices = swits 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end 
+			unless temps == nil 
+             @user = User.find(@thing_user)
+             devices = temps 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end 
+			unless hums == nil 
+             @user = User.find(@thing_user)
+             devices = hums 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end
+			unless bats == nil 
+             @user = User.find(@thing_user)
+             devices = bats 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end 
+			unless conts == nil 
+             @user = User.find(@thing_user)
+             devices = conts 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end 
+			unless illums == nil 
+             @user = User.find(@thing_user)
+             devices = illums 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end
+			unless mots == nil 
+             @user = User.find(@thing_user)
+             devices = mots 
+             devices.each do |it|
+        
+			        stuff = Thing.find_or_initialize_by(device_type: it["type"], uid: it["id"])
+			        stuff.uid = it["id"]
+					stuff.device_type = it["type"]
+					stuff.label = it["name"]
+					stuff.provider = "smartthings"
+					stuff.user = @user
+					stuff.save
+			  end
+			
+			end     
 		
 			
 		end 
