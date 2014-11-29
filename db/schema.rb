e7cbb1327adb4fd5a86283e97f98a610463895c9
Jody Albritton <jody.albritton@gmail.com>
2014-11-28 22:45:17 -0600

    add time zone support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124234839) do
+ActiveRecord::Schema.define(version: 20141129040416) do
 
   create_table "events", force: true do |t|
     t.string   "date"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20141124234839) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "full_name"
+    t.string   "time_zone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
